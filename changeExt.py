90f0737815aed3892dc8e6ed82d261f7389dd78a
Alexander Bieniek <A.Bieniek789@gmail.com>
2017-10-03 16:15:36 -0500

    fixed extension modification when the labels overlap

--- changeExt.py
+++ changeExt.py
@@ -12,10 +12,12 @@ if (sys.argv[1][0] != '.' or sys.argv[2][0] != '.'):
     printusage()
 
 oldext = sys.argv[1]
+lenoldext = len(oldext)
 newext = sys.argv[2]
+lennewext = len(newext)
 directory = sys.argv[3]
 
 for filename in os.listdir(directory):
-    if (filename.find(newext) == -1 and filename.find(oldext) != -1):
+    if (filename.find(oldext) + lenoldext == len(filename)):
         extidx =  filename.find(oldext)
         os.rename(directory + '/' + filename, directory + '/' + filename[0:extidx] + newext)
